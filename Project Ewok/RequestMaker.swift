4921f2083570a1584f4a0337ddc175b00785f308
Benjamin Carlson <carlson.j.ben@gmail.com>
2016-08-08 15:16:56 -0400

    Added some commenting

--- Project Ewok/RequestMaker.swift
+++ Project Ewok/RequestMaker.swift
@@ -9,15 +9,31 @@
 import Foundation
 
 class RequestMaker{
+    /**
+     Basic usage:
+     use one of the constructors to make the request
+     Always be sure to include a URL component
+     
+     Call the run function, either leaving arguments empty
+     or adding a "completion" argument with a function that takes
+     NSData as an argument
+     
+     The run function will asynchronously update the JSONData and
+     set ready to true upon completion.
+     
+     If a function was used as an argument for run, that function
+     will be called when the task is completed.
+    */
+    
     //Properties
-    var requestURL: NSURL;
-    var request: NSMutableURLRequest;
-    var JSONData: NSData?;
-    var ready: Bool?;
+    var requestURL: NSURL;              //The request's url
+    var request: NSMutableURLRequest;   //The request itslef
+    var JSONData: NSData?;              //The data returned from the request
+    var ready: Bool?;                   //Whether or not the data is ready to use
     
     //Constants
-    let Session = NSURLSession.sharedSession();
-    let BaseURL = "http://chitna.asap.um.maine.edu/projectcrowdsource/public/api/";
+    let Session = NSURLSession.sharedSession(); //The session
+    let BaseURL = "http://chitna.asap.um.maine.edu/projectcrowdsource/public/api/"; //The base URL for the API
     
     //Constructor
     init(method: String, url: String, data: NSData?){
@@ -46,6 +62,10 @@ class RequestMaker{
     
     //Function
     internal func run(completion: ((NSData) -> Void)?){
+        //PRE: if completion is included, completion MUST take a single NSData argument and return void
+        //POST: runs the request and updates JSONData. Sets ready to true when completed. Runs the
+        //      completion argument once the request completes if completion is included.
+        ready = false;
         let task = Session.dataTaskWithRequest(request, completionHandler:  {
             (data, response, error)->Void in
             
