d78225377fc15676795f5ff5a0e330f00e280f4d
jacobhall <jacob.hall1@maine.edu>
2016-08-12 12:13:42 -0400

    Started creating the results views

---
+++ Project Ewok/MainContentCell.swift
@@ -0,0 +1,13 @@
+//
+//  MainContentCell.swift
+//  Project Ewok
+//
+//  Created by Jacob Hall on 8/12/16.
+//  Copyright © 2016 ASAP. All rights reserved.
+//
+
+import UIKit
+
+class MainContentCell: UITableViewCell {
+
+}
--- ResultsViewController.swift
+++ ResultsViewController.swift
@@ -8,8 +8,67 @@
 
 import UIKit
 
-class ResultsViewController: UIViewController {
+class ResultsViewController: UITableViewController{
     
     var LocationId = Int()
+    
+    var results = [ReviewModel]()
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        var api = ApiInterface()
+        
+        api.getReviews(LocationId)
+        
+        while api.returns == nil {
+            
+            sleep(1)
+        }
+        
+        var results = api.returns
+        
+        var hi = results[1]
+        
+        hi.
+        
+        
+    }
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if section == 0 {
+            
+            return 1
+            
+        }else if section == 1{
+            
+            return 2
+            
+        }else {
+            
+            return 0
+            
+        }
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        if indexPath.section == 0 {
+            
+            
+            
+        }else if indexPath.section == 1{
+            
+            
+            
+        }
+        
+        return UITableViewCell()
 
+    }
+    
+    
+    
+    
 }
