fb3d297a1bf3e63152f6218db43daf26598d7319
jacobhall <jacob.hall1@maine.edu>
2016-08-17 11:45:34 -0400

    Minor Changes

---
+++ Project Ewok/FilterData.swift
@@ -0,0 +1,21 @@
+//
+//  FilterData.swift
+//  Project Ewok
+//
+//  Created by Jacob Hall on 8/17/16.
+//  Copyright © 2016 ASAP. All rights reserved.
+//
+
+import Foundation
+
+class FilterData {
+    
+    static let sharedFilterData = FilterData()
+    
+    var rating : Double?
+    
+    var distance : Int?
+    
+    var
+    
+}
